4b146df102c421a55d9d9d4cf960285a348fd4f1
Jordan Kline <jordan.kline@gmail.com>
2018-02-28 11:58:59 -0800

    added new location functionality and fixed location presentation in some views

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,10 +1,19 @@
 class LocationsController < ApplicationController
 
 	get '/locations' do
-		@locations = Location.all
 
 		erb :'/locations/index'
 	end
 
+	get '/locations/new' do
+
+		erb :'/locations/new'
+	end
+
+	post '/locations/new' do
+		@location = Location.create(params[:location])
+		binding.pry
+		redirect to "/locations"
+	end
 
 end
